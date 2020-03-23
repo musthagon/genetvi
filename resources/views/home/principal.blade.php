ee9f32f8906b8460005b1fa7f94d671ebf03a06d
miguelangelmagdalena <miguelangelmagdalena@gmail.com>
2020-03-22 21:15:26 -0400

    Agregando instrucción para docentes en Genially

--- resources/views/home/principal.blade.php
+++ resources/views/home/principal.blade.php
@@ -80,6 +80,11 @@
               <li>Contribuir a evaluar otros EVA a los que han sido invitados.</li>
             </ol>
             
+            <div class="container-wrapper-genially" style="position: relative; min-height: 400px; max-width: 100%;">
+            <img src="https://genially.blob.core.windows.net/genially/version3.0/loader.gif" class="loader-genially" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0; margin-top: auto; margin-right: auto; margin-bottom: auto; margin-left: auto; z-index: 1;width: 80px; height: 80px;"/>
+            <div id="5e77f8a701bd350de7aebd89" class="genially-embed" style="margin: 0px auto; position: relative; height: auto; width: 100%;"></div></div>
+            
+
         </div>
         <!-- /.tab-pane -->
         <div class="tab-pane" id="tab_2_2">
@@ -130,6 +135,7 @@
 @stop
 
 @section('javascript')
+  <script>(function (d) { var js, id = "genially-embed-js", ref = d.getElementsByTagName("script")[0]; if (d.getElementById(id)) { return; } js = d.createElement("script"); js.id = id; js.async = true; js.src = "https://view.genial.ly/static/embed/embed.js"; ref.parentNode.insertBefore(js, ref); }(document));</script> 
   <script>
     $(function (){
       
